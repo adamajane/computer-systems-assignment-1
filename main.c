7e0ac7922672d5665108daa9e5e29475d7dc9eeb
adamajane <adamajane@gmail.com>
2022-09-16 22:19:47 +0200

    Added erosion function and call and formatted code

--- main.c
+++ main.c
@@ -1,83 +1,104 @@
-//To compile (linux/mac): gcc cbmp.c main.c -o main.out -std=c99
-//To run (linux/mac): ./main.out example.bmp example_inv.bmp
+// To compile (linux/mac): gcc cbmp.c main.c -o main.out -std=c99
+// To run (linux/mac): ./main.out example.bmp example_inv.bmp
 
-//To compile (win): gcc cbmp.c main.c -o main.exe -std=c99
-//To run (win): main.exe example.bmp example_inv.bmp
+// To compile (win): gcc cbmp.c main.c -o main.exe -std=c99
+// To run (win): main.exe example.bmp example_inv.bmp
 
 #include <stdlib.h>
 #include <stdio.h>
 #include "cbmp.h"
 
-//Function to invert pixels of an image (negative)
- void invert(unsigned char input_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS], unsigned char output_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS]){
+// Function to invert pixels of an image (negative)
+void invert(unsigned char input_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS], unsigned char output_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS])
+{
   for (int x = 0; x < BMP_WIDTH; x++)
   {
     for (int y = 0; y < BMP_HEIGTH; y++)
     {
       for (int c = 0; c < BMP_CHANNELS; c++)
       {
-      output_image[x][y][c] = 255 - input_image[x][y][c];
+        output_image[x][y][c] = 255 - input_image[x][y][c];
       }
     }
   }
 }
 
 // This function converts the image to black and white (applies the binary threshold)
- void convert_blackwhite(unsigned char output_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS], unsigned char blackwhite_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS]){
+void convert_blackwhite(unsigned char output_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS], unsigned char blackwhite_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS])
+{
   int res = 0;
   for (int x = 0; x < BMP_WIDTH; x++)
   {
     for (int y = 0; y < BMP_HEIGTH; y++)
     {
-      if ((output_image[x][y][0] + output_image[x][y][1] + output_image[x][y][2]) /3 > 130){
-      blackwhite_image[x][y][0] = 0;
-      blackwhite_image[x][y][1] = 0;
-      blackwhite_image[x][y][2] = 0;
+      if ((output_image[x][y][0] + output_image[x][y][1] + output_image[x][y][2]) / 3 > 130)
+      {
+        blackwhite_image[x][y][0] = 0;
+        blackwhite_image[x][y][1] = 0;
+        blackwhite_image[x][y][2] = 0;
       }
-      else {
-      blackwhite_image[x][y][0] = 255;
-      blackwhite_image[x][y][1] = 255;
-      blackwhite_image[x][y][2] = 255;
+      else
+      {
+        blackwhite_image[x][y][0] = 255;
+        blackwhite_image[x][y][1] = 255;
+        blackwhite_image[x][y][2] = 255;
       }
-    
     }
   }
 }
 
+// This function erodes the black and white image using binary erosion
 
-  //Declaring the array to store the image (unsigned char = unsigned 8 bit)
-  unsigned char input_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS];
-  unsigned char output_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS];
-  unsigned char blackwhite_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS];
-
-//Main function
-int main(int argc, char** argv)
+void erode_image()
 {
-  //argc counts how may arguments are passed
-  //argv[0] is a string with the name of the program
-  //argv[1] is the first command line argument (input image)
-  //argv[2] is the second command line argument (output image)
+  // Initializes the structuring element  
 
-  //Checking that 2 arguments are passed
+  int structuring_element[3][3] = {{0, 1, 0}, {1, 1, 1}, {0, 1, 0}};
+
+  for (int i = 0; i < 3;i++){
+    for (int j = 0; j < 3;j++)
+      printf("Matrix element:%d\n",structuring_element[i][j]);
+  }
+
+
+}
+
+// Declaring the array to store the image (unsigned char = unsigned 8 bit)
+unsigned char input_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS];
+unsigned char output_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS];
+unsigned char blackwhite_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS];
+
+// Main function
+int main(int argc, char **argv)
+{
+  // argc counts how may arguments are passed
+  // argv[0] is a string with the name of the program
+  // argv[1] is the first command line argument (input image)
+  // argv[2] is the second command line argument (output image)
+
+  // Checking that 2 arguments are passed
   if (argc != 3)
   {
-      fprintf(stderr, "Usage: %s <output file path> <output file path>\n", argv[0]);
-      exit(1);
+    fprintf(stderr, "Usage: %s <output file path> <output file path>\n", argv[0]);
+    exit(1);
   }
 
   printf("Example program - 02132 - A1\n");
 
-  //Load image from file
+  // Load image from file
   read_bitmap(argv[1], input_image);
 
-  //Run inversion
-  invert(input_image,output_image);
+  // Run inversion
+  invert(input_image, output_image);
 
-  //Run blackwhite conversion
+  // Run blackwhite conversion
   convert_blackwhite(output_image, blackwhite_image);
 
-  //Save image to file
+  // Save image to file
   write_bitmap(blackwhite_image, argv[2]);
+
+  // Runs the erosion
+  erode_image();
 
   printf("Done!\n");
   return 0;
